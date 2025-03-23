d93d035f5fda7d9fe467919b7182e11d22b555c1
René <renestodola@gmail.com>
2025-03-23 21:32:28 +0100

    Enhance configuration management by adding environment variable parsing and improving config loading logic

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import json
 import os
 import shutil
+from dotenv import load_dotenv
 
 """
 Usage:
@@ -10,12 +11,17 @@ Usage:
 	CONFIG = CONFIG_INSTANCE.CONFIG
 	CONFIG["key"] = "value"
 """
+
+
 class Config:
 	def __init__(self) -> None:
 		"""Initializes the configuration."""
 		self._config_file = "config/config.json"
 		self._template_file = "config/config.init.json"
+		self.VARIABLES = {}
+		self.CONFIG = {}
 		self.load_config()
+		self.parse_env()
 
 	def load_config(self) -> None:
 		"""Loads configuration from file. If it doesn't exist, copies from template."""
@@ -31,7 +37,8 @@ class Config:
 		with open(self._config_file, "r") as f:
 			config = json.load(f)
 
-		self.CONFIG = config
+		self.CONFIG.clear()
+		self.CONFIG.update(config)
 
 	def save_config(self) -> None:
 		"""Saves the current configuration to file."""
@@ -39,12 +46,50 @@ class Config:
 		with open(self._config_file, "w") as f:
 			json.dump(config, f, indent=4)
 
+	def parse_env(self) -> None:
+		"""Parses environment variables and updates the configuration."""
+		# Expected format in .env: USERS=admin:password123,user:pass456
+		if os.path.exists('.env'):
+			load_dotenv('.env')
+		else:
+			print("Warning: .env file not found. No user authentication data loaded.")
+		users_env = os.getenv("USERS", "")
+		self.USERS = {}
+		if users_env:
+			for user_entry in users_env.split(","):
+				try:
+					username, password = user_entry.split(":")
+					self.USERS[username.strip()] = password.strip()
+				except Exception as e:
+					print(f"Error parsing user entry '{user_entry}': {e}")
+		self.KISMET_USER = os.getenv("KISMET_USER", "")
+		self.KISMET_PASSWORD = os.getenv("KISMET_PASS", "")
+
+	def get_logger_settings(self) -> tuple:
+		"""
+		Returns the logger settings.
+		
+		Returns:
+			tuple: Logger settings
+		"""
+		return (
+			self.CONFIG["logger"]["name"],
+			self.CONFIG["logger"]["file"],
+			self.CONFIG["logger"]["file_level"],
+			self.CONFIG["logger"]["console_level"]
+		)
+
+
+_config_instance = None
 
 def get_config() -> Config:
-	"""
-	Returns a Config instance.
-	
-	Returns:
-		Config: Config instance
-	"""
-	return Config()
+    """
+    Returns a singleton Config instance.
+    
+    Returns:
+        Config: Config instance
+    """
+    global _config_instance
+    if _config_instance is None:
+        _config_instance = Config()
+    return _config_instance
